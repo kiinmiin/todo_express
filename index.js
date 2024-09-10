2e0112be2502f981effdc0d55bad67ed8b070065
kiinmiin <rasmo.kiin@voco.ee>
2024-09-10 20:49:20 +0300

    Write data to file

--- index.js
+++ index.js
@@ -7,19 +7,50 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
-    // Get data from the file
-    fs.readFile('./tasks', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-            // Task list data from file
-        const tasks = data.split("\n") 
-        res.render('index', {tasks:tasks})
-    });
-})
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        // Get data from the file
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            // Task list data from the file
+            const tasks = data.split("\n")
+            resolve(tasks) 
+        });
+    })
+} 
 
+app.get('/', (req, res) => {
+    // Task list data from file
+    readFile('./tasks')
+    .then(tasks => {
+        console.log(tasks)
+        res.render('index', {tasks: tasks})
+    })
+}) 
+
+// For parsing application
+app.use(express.urlencoded({ extended: true}));
+
+app.post('/', (req, res) => {
+    // Tasks list data from file
+    readFile('./tasks')
+        .then(tasks => {
+            // Add form sent task to tasks array
+            tasks.push(req.body.task)
+            const data = tasks.join("\n")
+            fs.writeFile('./tasks', data, err => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                // Redirect to / to see result
+                res.redirect('/') 
+            })
+    })
+})
 
 app.listen(3001, () => { 
     console.log('Example app is started at http://localhost:3001')
